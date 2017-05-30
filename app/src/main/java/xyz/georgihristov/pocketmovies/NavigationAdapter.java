08aeeda719318d51dd55a51212ad56f9ea7ad9cb
gohv <hristovosotirovgeorgi@gmail.com>
2017-05-30 21:19:02 +0300

    categories added, reuse asynctast,better layout

---
+++ app/src/main/java/xyz/georgihristov/pocketmovies/NavigationAdapter.java
@@ -0,0 +1,10 @@
+package xyz.georgihristov.pocketmovies;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by gohv on 30.05.17.
+ */
+
+public class NavigationAdapter extends ArrayAdapter<String> {
+}
