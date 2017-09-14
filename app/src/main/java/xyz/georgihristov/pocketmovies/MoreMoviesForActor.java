31b3ee6dda9e40d09b9672752402423313410cdf
Hristov <georgi.hristov@accenture.com>
2017-09-14 15:08:30 +0300

    More Movies for actors view

---
+++ app/src/main/java/xyz/georgihristov/pocketmovies/MoreMoviesForActor.java
@@ -0,0 +1,8 @@
+package xyz.georgihristov.pocketmovies;
+
+/**
+ * Created by georgi.hristov on 9/14/2017.
+ */
+
+public class MoreMoviesForActor {
+}
--- app/src/main/java/xyz/georgihristov/pocketmovies/MoreMoviesResult.java
+++
@@ -1,8 +0,0 @@
-package xyz.georgihristov.pocketmovies;
-
-/**
- * Created by georgi.hristov on 9/12/2017.
- */
-
-public class MoreMoviesResult {
-}
